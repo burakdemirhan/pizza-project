91466503d832f486f0db4d09a3dd4e72f0372e5f
burakdemirhan <burkidad@outlook.com>
2024-10-09 19:42:48 +0300

    siparis commit

--- src/components/Siparis.jsx
+++ src/components/Siparis.jsx
@@ -11,7 +11,9 @@ export default function Siparis() {
     return (
         <div className="siparis-page">
             <h1 class="tebrik">TEBRİKLER ! <br />SİPARİŞİNİZ ALINDI</h1>
+            <h1>Merhaba</h1>
         </div>
+      
     )
 
 

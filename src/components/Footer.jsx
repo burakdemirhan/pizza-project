49afebed0b8f55b4507b7c45b7268ce335fa1a1d
burakdemirhan <burkidad@outlook.com>
2024-10-11 15:57:27 +0300

    anasayfanın tüm cssleri yapıldı footer bölümü tamamlandı ve her sayfanın altında görünmesi saglandı

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,53 +5,61 @@ export default function Footer(){
 
 
     return(
-        <div className="footer-container">
-            
+        <footer>
+         <div className="footer-content">
          <div className="footer-first">
-         <h1>Teknolojik <br /> Yemekler</h1>
-            <img src="/footer/icons/icon-1.png" alt="adres"/>
+         <h1 className="footer-title">Teknolojik <br /> Yemekler</h1>
+            <div className="footer-item">
+         
+            <img src="/src/assets/icon-1.png" alt="adres"/>
            <p>341 Londonderry Road, <br />
            Istanbul Türkiye</p>
-           <img src="/footer/icons/icon-2.png" alt="mail" />
+           </div>
+           <div className="footer-item">
+           <img src="/src/assets/icon-2.png" alt="mail" />
            <p>aciktim@teknolojikyemekler.com</p>
-           <img src="/footer/icons/icon-3.png" alt="telefon" />
+           </div>
+           <div className="footer-item">
+           <img src="/src/assets/icon-3.png" alt="telefon" />
            <p>+90 216 123 45 67</p>
+           </div>
          </div>
          <div className="footer-second">
-         <h2>Hot Menu</h2>
-         <ul>
-            <li>Terminal Pizza</li>
-            <li>5 Kişilik Hackathlon Pizza</li>
-            <li>useEffect Tavuklu Pizza</li>
-            <li>Beyaz  Console Frosty</li>
-            <li>Testler Geçti Mutlu Burger</li>
-            <li>Position Absolute Acı Burger</li>
+         <p>Hot Menu</p>
+       
+            <p>Terminal Pizza</p>
+            <p>5 Kişilik Hackathlon Pizza</p>
+            <p>useEffect Tavuklu Pizza</p>
+            <p>Beyaz  Console Frosty</p>
+            <p>Testler Geçti Mutlu Burger</p>
+            <p>Position Absolute Acı Burger</p>
         
-         </ul>
     
          </div>
          <div className="footer-third">
-            <h2>Instagram</h2>
+            <p>Instagram</p>
              <div className="img-first">
 
-             <img src="/footer/insta/li-0.png" />
-            <img src="/footer/insta/li-1.png" />
-            <img src="/footer/insta/li-2.png" />
+             <img src="/src/assets/li-0.png" />
+            <img src="/src/assets/li-1.png" />
+            <img src="/src/assets/li-2.png" />
 
              </div>
             <div className="img-second">
-            <img src="/footer/insta/li-3.png" />
-            <img src="/footer/insta/li-4.png" />
-            <img src="/footer/insta/li-5.png" />
+            <img src="/src/assets/li-3.png" />
+            <img src="/src/assets/li-4.png" />
+            <img src="/src/assets/li-5.png" />
 
+         
             </div>
-            
+            </div>
            
          </div>
          <div className="son">
             <p>© 2023 Teknolojik Yemekler.</p>
+            <i class="fa fa-twitter"></i>
          </div>
          
-        </div>
+        </footer>
     )
 }
